0cd2d1b610ce935610bde578ceebe5672f28bf42
Gabe Lapingcao <mgultra2018@gmail.com>
2025-02-26 13:11:42 -0800

    Initial commit

---
+++ src/main/java/big/census/big_smallville_census_api/BigSmallvilleCensusApiApplication.java
@@ -0,0 +1,13 @@
+package big.census.big_smallville_census_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigSmallvilleCensusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigSmallvilleCensusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/big/census/big_smallville_census_api/BigSmallvilleCensusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package big.census.big_smallville_census_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigSmallvilleCensusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
