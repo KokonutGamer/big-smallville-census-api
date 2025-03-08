54dd7a7eef0c93d3f5a24bc76af844457abad708
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-08 11:14:41 -0800

    Implemented district

---
+++ src/main/java/big/census/big_smallville_census_api/entities/District.java
@@ -0,0 +1,20 @@
+package big.census.big_smallville_census_api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
+@Table(name = "district")
+public class District {
+    @Id
+    @Column(columnDefinition = "BPCHAR(2)")
+    private String id;
+
+    @NonNull
+    private String name;
+}
