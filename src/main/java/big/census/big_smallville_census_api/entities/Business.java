948ca6b2c7e9ca00f2b6d9ae6b8152629beb83f1
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-08 11:44:35 -0800

    Implemented business entity class

---
+++ src/main/java/big/census/big_smallville_census_api/entities/Business.java
@@ -0,0 +1,38 @@
+package big.census.big_smallville_census_api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
+@Table(name = "business")
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    private String name;
+
+    private String email;
+
+    @Column(columnDefinition = "BPCHAR(10)")
+    private String phone;
+
+    @NonNull
+    private String street;
+
+    @NonNull
+    @Column(name = "zipcode", columnDefinition = "BPCHAR(5)")
+    private String zipCode;
+
+    @NonNull
+    @Column(name = "buildingnumber")
+    private String buildingNumber;
+}
