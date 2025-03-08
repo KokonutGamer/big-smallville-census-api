2007582a317e66038940aa64801074e572586b85
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-08 11:12:19 -0800

    Added martial status and refactored person entity

---
+++ src/main/java/big/census/big_smallville_census_api/entities/MaritalStatus.java
@@ -0,0 +1,18 @@
+package big.census.big_smallville_census_api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
+@Table(name = "maritalstatus")
+public class MaritalStatus {
+    @Id
+    private Character id;
+
+    @NonNull
+    private String name;
+}
--- src/main/java/big/census/big_smallville_census_api/entities/Person.java
+++ src/main/java/big/census/big_smallville_census_api/entities/Person.java
@@ -2,26 +2,42 @@ package big.census.big_smallville_census_api.entities;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NonNull;
 
 @Entity
 @Data
+@Table(name = "person")
 public class Person {
-    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Integer id;
-    private @NonNull String SSN;
-    private @NonNull String maritalstatusid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @Column(columnDefinition = "BPCHAR(9)")
+    private String SSN;
+
+    @NonNull
+    private Character maritalstatusid;
     
     // TODO implement Household entity with @ManyToOne mapping
 
-    private @NonNull String firstname;
-    private @NonNull String lastname;
-    private @NonNull Date birthdate;
+    @NonNull
+    private String firstname;
+
+    @NonNull
+    private String lastname;
+
+    @NonNull
+    private Date birthdate;
+
     private String email;
     private String phone;
 }
