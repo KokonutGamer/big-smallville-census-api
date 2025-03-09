ba0b6da6cdfc4c07f88391db01a058f1ce45bf06
Gabe Lapingcao <mgultra2018@gmail.com>
2025-03-08 17:22:54 -0800

    Outlined property type service and repository

---
+++ src/main/java/big/census/big_smallville_census_api/repositories/PropertyTypeRepository.java
@@ -0,0 +1,6 @@
+package big.census.big_smallville_census_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyTypeRepository extends JpaRepository<PropertyTypeRepository, String> {
+}
---
+++ src/main/java/big/census/big_smallville_census_api/services/PropertyTypeService.java
@@ -0,0 +1,8 @@
+package big.census.big_smallville_census_api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyTypeService {
+    /* TODO implement service */
+}
