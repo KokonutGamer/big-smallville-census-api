e1e3227e6ae49fc0016b04eee6fa4dd3ab4eaba3
D4KRLE <d4klre@outlook.com>
2025-03-10 17:32:43 -0700

    implemented listEmployeesInABusiness method

--- src/main/java/big/census/big_smallville_census_api/repositories/BusinessRepository.java
+++ src/main/java/big/census/big_smallville_census_api/repositories/BusinessRepository.java
@@ -1,8 +1,15 @@
 package big.census.big_smallville_census_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import big.census.big_smallville_census_api.entities.Business;
+import big.census.big_smallville_census_api.entities.Employee;
 
 public interface BusinessRepository extends JpaRepository<Business, Integer> {
+  @Query(nativeQuery = true, value = "SELECT P.firstName, P.lastName, E.income FROM Person AS P JOIN Employee AS E ON (P.ID = E.personID) JOIN Business AS B ON (B.ID = E.businessID) WHERE B.name = :businessName;")
+  List<Employee> getEmployeesInABusiness(@Param("businessName") String businessName);  
 }
--- src/main/java/big/census/big_smallville_census_api/services/BusinessService.java
+++ src/main/java/big/census/big_smallville_census_api/services/BusinessService.java
@@ -1,8 +1,20 @@
 package big.census.big_smallville_census_api.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import big.census.big_smallville_census_api.entities.Employee;
+import big.census.big_smallville_census_api.repositories.BusinessRepository;
 
 @Service
 public class BusinessService {
     /* TODO implement service */
+    @Autowired
+    private BusinessRepository businessRepository;
+
+    List<Employee> listEmployeesInABusiness(String businessName){
+        return businessRepository.getEmployeesInABusiness(businessName);
+    }
 }
