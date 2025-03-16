6fed37e26c11f60825f91fb9d7b1fb39f77af5df
D4KRLE <d4klre@outlook.com>
2025-03-16 00:15:37 -0700

    changed @GetMapping of updateTaxPercentage to @PutMapping, updated test commands

--- src/main/java/big/census/big_smallville_census_api/controllers/PropertyTypeController.java
+++ src/main/java/big/census/big_smallville_census_api/controllers/PropertyTypeController.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,18 +27,16 @@ public class PropertyTypeController{
   /**
    * Update the tax percentage based on a given property type name
    * 
-   * Test on
-   * http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Vehicle&newTaxPercentage=0.8
+   * Test by running the following code in bash
+   * curl -X PUT "http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Vehicle&newTaxPercentage=0.8"
    * The given property type name is 'Vehicle' and new tax percentage is 0.8
    * This should return 1
    * 
-   * Test on
-   * http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Air&newTaxPercentage=0.8
+   * curl -X PUT "http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Air&newTaxPercentage=0.8"
    * The given property type name is 'Air'
    * This should return 0
    * 
-   * Test on
-   * http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Vehicle&newTaxPercentage=-1
+   * curl -X PUT "http://localhost:8080/api/v1/propertyType/updateTaxPercentage?propertyTypeName=Vehicle&newTaxPercentage=-1"
    * The new percentage is -1
    * This should return 0
    * 
@@ -45,7 +44,7 @@ public class PropertyTypeController{
    * 
    * @author Ting Gao
    */
-  @GetMapping("/updateTaxPercentage")
+  @PutMapping("/updateTaxPercentage")
   ResponseEntity<TaxPercentageUpdateResponse> updateTaxPercentage(@RequestParam BigDecimal newTaxPercentage, @RequestParam String propertyTypeName) {
     TaxPercentageUpdateResponse result = new TaxPercentageUpdateResponse(propertyTypeService.updateTaxPercentageForASpecificPropertyType(newTaxPercentage, propertyTypeName));
     return ResponseEntity.ok(result);
