80bea1c34cd6a79ba9ddd677f3c4bb363eb23f5d
David P <davidhunterphillips03@gmail.com>
2025-03-16 14:27:20 -0700

    added comments for my controller funcs, added demo inputs to readME

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## Demo Inputs ##
+## Demo Inputs
+
+listBusinessRecords: "Serious Business"
+
+updateMinWage: "Serious Business" and 70000
--- src/main/java/big/census/big_smallville_census_api/controllers/BusinessController.java
+++ src/main/java/big/census/big_smallville_census_api/controllers/BusinessController.java
@@ -32,22 +32,38 @@ public class BusinessController {
     return ResponseEntity.ok(result);
   }
 
-  // test on
-  // http://localhost:8080/api/v1/businesses/listBusRecords?businessName=Serious%20Business
+  /**
+   * @author David Phillips
+   * @description returns a list of business records form a given business name
+   * @param businessName
+   * @return BusinessRecordResponse: a wrapper class which holds a list of
+   *         business
+   *         objects
+   * @testOn http://localhost:8080/api/v1/businesses/listBusRecords?businessName=Serious%20Business
+   */
   @GetMapping("/listBusRecords")
   ResponseEntity<BusinessRecordResponse> listBusinessRecords(@RequestParam String businessName) {
     BusinessRecordResponse result = new BusinessRecordResponse(businessService.listRecordsOfABusiness(businessName));
     return ResponseEntity.ok(result);
   }
 
+  /**
+   * @author David Phillips
+   * @description updates all employee wages of a company below a threshold to be
+   *              at that threshold
+   * @param businessName, newWage:
+   * @return WageUpdateResponse: A count of the employees updated
+   * @testOn http://localhost:8080/api/v1/businesses/updateMinWage?businessName=Serious%20Business&newWage=70000
+   */
   @GetMapping("/updateMinWage")
-  // http://localhost:8080/api/v1/businesses/updateMinWage?businessName=Serious%20Business&newWage=70000
   ResponseEntity<WageUpdateResponse> updateMinWage(@RequestParam String businessName, @RequestParam Integer newWage) {
     WageUpdateResponse result = new WageUpdateResponse(businessService.updateMinWage(businessName, newWage));
     return ResponseEntity.ok(result);
   }
+
   /**
-   * Show first name, last name, and income of all employees based on a given business name
+   * Show first name, last name, and income of all employees based on a given
+   * business name
    * 
    * Test on
    * http://localhost:8080/api/v1/businesses/listEmployees?businessName=Serious%20Business
